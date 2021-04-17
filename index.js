057e46a302204ae9375a9386102d23744c944959
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-04-17 17:13:10 +0530

    chore: setup progressive rendering using express-stream

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const compression = require('compression');
+const stream = require('express-stream');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.set('view engine', 'pug');
+app.use(compression());
+app.use(express.static('public'));
+
+app.use(function cache(req, res, next) {
+  res.setHeader('Cache-Control', 'public, max-age=5000');
+  next();
+});
+
+app.use(function contentType(req, res, next) {
+  res.setHeader('Content-Type', 'text/html');
+  next();
+});
+
+
+stream.openHtmlOpenHead(true);
+//stream.closeHeadOpenBody('_head');
+stream.closeBodyCloseHtml(true);
+stream.streamBefore('_header');
+stream.streamAfter('_footer');
+
+
+app.get('/', stream.stream(), (req,res) => {
+  res.render('index');
+});
+
+app.get('/values', stream.stream(), (req,res) => {
+  res.render('values');
+});
+
+app.get('/principles', stream.stream(), (req,res) => {
+  res.render('principles');
+});
+
+app.get('/practices', stream.stream(), (req,res) => {
+  res.render('practices');
+});
+
+app.listen(PORT);
+console.log('Listening on port: ', PORT);
+
--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./api/index');
-
-const PORT = process.env.port || 3000;
-app.listen(3000);
-
-console.log('Listening on port: ', 3000);
