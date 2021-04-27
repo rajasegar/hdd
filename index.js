bf67c2de28d158ccb8cc56569cbf3902d55371bf
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-04-27 09:08:13 +0530

    chore: responsive fixes
    1. remove roboto font
    2. add bootstrap js
    3. add bg color for log

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const compression = require('compression');
 const stream = require('express-stream');
-const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,27 +11,9 @@ const valuesRouter = require('./routes/values');
 app.set('view engine', 'pug');
 app.use(compression());
 app.use(express.static('public'));
-app.use(cookieParser('html-driven-dev'));
-
-// Disable cache control for now
-/*
-app.use(function cache(req, res, next) {
-  res.setHeader('Cache-Control', 'public, max-age=5000');
-  next();
-});
-*/
 
 app.use(function contentType(req, res, next) {
   res.setHeader('Content-Type', 'text/html');
-  next();
-});
-
-app.use(function (req, res, next) {
-  if (req.cookies.darkMode) {
-    res.locals.darkMode = req.cookies.darkMode;
-  } else {
-    res.cookie('darkMode', 1, { maxAge: 900000, httpOnly: true, overwrite: true });
-  }
   next();
 });
 
@@ -41,7 +22,6 @@ stream.streamBefore('_header');
 stream.streamAfter('_footer');
 
 app.get('/', stream.stream(), (req, res) => {
-  console.log(req.cookies);
   res.render('index');
 });
 
@@ -59,18 +39,6 @@ app.get('/tools', stream.stream(), (req, res) => {
 
 app.use('/practices', practicesRouter);
 app.use('/values', valuesRouter);
-
-app.get('/light-mode', (req, res) => {
-  console.log(req.cookies);
-  res.cookie('darkMode', 0, { maxAge: 900000, httpOnly: true, overwrite: true });
-  res.redirect('/');
-});
-
-app.get('/dark-mode', (req, res) => {
-  console.log(req.cookies);
-  res.cookie('darkMode', 1, { maxAge: 900000, httpOnly: true, overwrite: true });
-  res.redirect('/');
-});
 
 app.get('/about', stream.stream(), (req, res) => {
   res.render('about');
